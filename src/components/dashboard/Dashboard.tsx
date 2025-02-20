48bc10968321608dad2229e35348defafc331886
Shraddha <mailmeatshraddha@gmail.com>
2025-02-20 16:31:06 -0500

    add default col def

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -10,15 +10,26 @@ type RowData = {
 };
 
 export const Dashboard = () => {
+  const defaultColDef: ColDef<RowData> = {
+    sortable: true,
+    sortingOrder: ['asc', null],
+  };
   const [columnDefs] = useState<ColDef<RowData>[]>([
-    { headerName: 'Product Name', field: 'name', sortable: true, sortingOrder: ['desc', 'asc'] },
+    { headerName: 'Product Name', field: 'name', sortingOrder: ['asc', 'desc'] },
     { headerName: 'Color', field: 'color' },
   ]);
   const [rowData] = useState<RowData[]>(data.products);
 
   return (
     <div className="ag-theme-material-auto-light ag-grid-container">
-      <AgGridReact debug theme={themeQuartz} columnDefs={columnDefs} rowData={rowData} multiSortKey="ctrl" />
+      <AgGridReact
+        debug
+        theme={themeQuartz}
+        defaultColDef={defaultColDef}
+        columnDefs={columnDefs}
+        rowData={rowData}
+        multiSortKey="ctrl"
+      />
     </div>
   );
 };
